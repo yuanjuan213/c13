7808a6d2116d24c6113cc00f8a385e2b36dac00c
yuanjuan213 <110069234+yuanjuan213@users.noreply.github.com>
2022-08-16 18:26:00 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+var cloud, cloudsGroup, cloudImage;
+
+
+
+var newImage;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+ 
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  // trex.addAnimation("collided",trex_collided)
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  console.log("Hola"+ 5)
+  
+}
+
+function draw() {
+  background(180);
+  
+  
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  
+  //aparecer nubes
+  spawnClouds();
+  spawnObstac();
+  drawSprites();
+}
+
+function spawnClouds() {
+  //escribir aquí el código para aparecer las nubes
+  if (frameCount % 60 === 0) {
+    cloud = createSprite(600,100,40,10);
+    cloud.addImage(cloudImage)
+    cloud.y = Math.round(random(10,60))
+    cloud.scale = 0.4;
+    cloud.velocityX = -3;
+    
+    
+    //asignar tiempo de vida a una variable
+    cloud.lifetime = 134
+    
+    //ajustar la profundidad
+    cloud.depth = trex.depth
+    trex.depth = trex.depth + 1;
+    
+    }
+    
+    function spawnObstac() {
+      /*if(frameCount % 60 === 0){
+        var obstacle = createSprite(600,165,10,40);
+        obstacle.velocityX=-6;
+
+      }*/
+    }
+  }
